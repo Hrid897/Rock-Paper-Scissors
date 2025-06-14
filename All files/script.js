03bb693d8b324901713e2a9e3766a4dc80de56a0
Hrid897 <143290428+Hrid897@users.noreply.github.com>
2025-06-14 14:40:32 +0600

    Add files via upload

---
+++ All files/script.js
@@ -0,0 +1,55 @@
+let score = {
+    win: 0, lose: 0, tie: 0
+}
+let result = '';
+
+update_score();
+
+function gameplay(user_move){
+    const random_num = Math.random();
+    let computer_move = '';
+    if(0<=random_num && random_num<1/3) computer_move = 'rock';
+    else if(1/3<=random_num && random_num<2/3) computer_move = 'paper';
+    else computer_move = 'scissors';
+
+    if(user_move==='rock'){
+        if(computer_move==='rock') result = 'Tie';
+        else if(computer_move==='paper') result = 'Lose';
+        else result = 'Win';
+    }
+    else if(user_move==='paper'){
+        if(computer_move==='rock') result = 'Win';
+        else if(computer_move==='paper') result = 'Tie';
+        else result = 'Lose';
+    }
+    else{
+        if(computer_move==='rock') result = 'Lose';
+        else if(computer_move==='paper') result = 'Win';
+        else result = 'Tie';
+    }
+
+
+    if(JSON.parse(localStorage.getItem('score')) !==null){
+        score = JSON.parse(localStorage.getItem('score'));
+    }
+
+    if(result==='Win') score.win++;
+    else if(result==='Lose') score.lose++;
+    else if(result==='Tie') score.tie++;
+    
+
+    document.querySelector('.js-result').innerHTML = `It's ${result}`;
+    document.querySelector('.js-moves').innerHTML = `You
+        <img src="images/${user_move}.png" alt="" class="move-icon">
+        <img src="images/${computer_move}.png" alt="" class="move-icon">
+        Computer`;
+
+    update_score();
+    localStorage.setItem('score', JSON.stringify(score));
+}
+
+
+function update_score(){
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}`;
+}
+
